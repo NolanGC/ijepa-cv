8a02bfe304421023f3e876338e10a31f19fdcb27
Nolan Clement <nolangc@node0045.inband>
2024-04-16 22:58:44 -0400

    reading for slurm

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import pprint
 import yaml
 
 from src.utils.distributed import init_distributed
-from src.train import main as app_main
+from src.train_geo import main as app_main
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
